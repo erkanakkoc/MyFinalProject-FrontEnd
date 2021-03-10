5e3c830593aae0710863f7ce8dc3c4b0b2220532
erkanakkoc <akkoc.erkan@gmail.com>
2021-03-11 02:29:17 +0300

    Update D17

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import {HttpClient} from '@angular/common/http';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
 
 @Component({
   selector: 'app-product',
@@ -6,23 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
-  product1 = { productId: 1, productName: 'Bardak', categoryId: 1, unitPrice: 5 }
-  product2 = { productId: 2, productName: 'Laptop', categoryId: 1, unitPrice: 5 }
-  product3 = { productId: 3, productName: 'Mouse', categoryId: 1, unitPrice: 5 }
-  product4 = { productId: 4, productName: 'Keyboard', categoryId: 1, unitPrice: 5 }
-  product5 = { productId: 5, productName: 'Camera', categoryId: 1, unitPrice: 5 }
-
-  products = [
-    this.product1,
-    this.product2,
-    this.product3,
-    this.product4,
-    this.product5
-  ];
-  constructor() { }
+  products:Product[] = [];
+  apiUrl= "https://localhost:44371/api/products/getall"
+  productResponseModel:ProductResponseModel={
+    data : this.products,
+    message :"",
+    success:true
+  };
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.httpClient.get<ProductResponseModel>(this.apiUrl).subscribe((response)=> {this.products = response.data});
   }
 
 }
